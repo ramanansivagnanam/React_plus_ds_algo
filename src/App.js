109770184dc418bf34b06f5776d38c890df5fd2e
Ramanan S <rams2@publicisgroupe.net>
2024-07-09 16:51:07 +0530

    react design patterns HOC | render prop pattern | compound patter |custom hooks pattern | composition pattern

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-
-import Input from "./DesignPatterns/RenderPropPattern/Input";
+import Tab from "./DesignPatterns/CompoundPattern/Tab";
 
 const App = () => {
- 
-  const multiplyBy10 = (value) => {
-    return  <div>Value multiplyBy10 from input {value * 10} </div>
-  }
+  const [currentIndex, updateCurrentIndex] = useState(0);
 
-  const showValue = (value) =>  <div>Value from input {value} </div>
- 
   return (
-    <>
-       <Input multiplyBy10 = {multiplyBy10} showValue={showValue} />
-      
-      
-    </>
+    <Tab currentTab={currentIndex} onChange={updateCurrentIndex}>
+      <Tab.TabContainer>
+        <Tab.HeadingContainer>
+          <Tab.Item label={"item 1"} index={0} />
+          <Tab.Item label={"item 2"} index={1} />
+          <Tab.Item label={"item 3"} index={2} />
+        </Tab.HeadingContainer>
+        <Tab.ContentContainer>
+          <Tab.Content index={0} >
+            <div>Tab content 1</div>
+          </Tab.Content>
+          <Tab.Content index={1}>
+            <div>Tab content 2</div>
+          </Tab.Content>
+          <Tab.Content index={2}>
+            <div>Tab content 3</div>
+          </Tab.Content>
+        </Tab.ContentContainer>
+      </Tab.TabContainer>
+    </Tab>
   );
 };
 
--- src/DesignPatterns/CompoundPattern/Tab.js
+++ src/DesignPatterns/CompoundPattern/Tab.js
@@ -1,18 +1,32 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext } from "react";
+import './Tab.css';
+export const TabContext = createContext();
 
-function Tab({ currentTab, onChange, children}) {
-  const TabContext = createContext(null);
-  return (
-    <TabContext.Provider>
-        {children}
-    </TabContext.Provider>
-  )
+function Tab({ currentTab, onChange, children }) {
+  return <TabContext.Provider value={{currentTab, onChange }}>{children}</TabContext.Provider>;
 }
 
 export default Tab;
 
-Tab.TabContainer = ({children}) => {
-    return (<div>
-         {children}
-    </div>)
+Tab.TabContainer = ({ children }) => {
+  return <div className="tabContainer">{children}</div>;
+};
+
+Tab.HeadingContainer = ({ children }) => {
+  return <div className="headingContainer">{children}</div>;
+};
+Tab.Item = ({ label, index }) => {
+  const { onChange, currentTab }  = useContext(TabContext);
+  const handleTabClick = () => {
+    onChange(index);
+  }
+  return <div className={`item ${currentTab === index ? 'active': ''}`} onClick={handleTabClick}>{label}</div>;
+};
+
+Tab.ContentContainer = ({ children }) => {
+    return <div className="contentContainer">{children}</div>;
+}
+Tab.Content = ({ children, index }) => {
+    const { currentTab } = useContext(TabContext);
+    return currentTab === index ? <div className="content">{children}</div> : null;
 }
